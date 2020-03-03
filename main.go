a4890a646b886e1a4efef3e4222eb9adfe2050a4
Raul Sanchez <raulsanchez1024@gmail.com>
2020-03-02 18:53:28 -0700

    Finished CRUD functions

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ type Book struct {
 	ID			string		`json:"id"`
 	ISBN		string		`json:"isbn"`
 	Title		string		`json:"title"`
-	Author	*Author		`json:"author"`
+	Author	*Author		``json:"author"`
 }
 
 // Author struct
@@ -52,12 +52,32 @@ func createBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
-
 func deleteBook(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
 }
 
 func main() {
